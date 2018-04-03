ce85708d7162261bdcbecd0005d8bb0a92ef0712
Krishna <kanha31@gmail.com>
2018-04-04 00:05:55 +0530

    Bug Fix for LSTM model

--- Lstm.py
+++ Lstm.py
@@ -1,19 +1,10 @@
-from keras.callbacks import LambdaCallback
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.layers import LSTM
-from keras.optimizers import RMSprop
-from keras.utils.data_utils import get_file
-import numpy as np
-import random
-import sys
-import io
 
-def LSTM(word_feature_size = 300,number_of_hidden_units = 1024,max_length = 30):
+def Lstm(word_feature_size = 300,number_of_hidden_units = 1024,max_length = 30):
     model = Sequential()
-    model.add(LSTM(output_size = number_of_hidden_units,return_sequences = True,input_shape=(max_length,word_feature_size)))
-    model.add(LSTM(output_size = number_of_hidden_units,return_sequences = False))
+    model.add(LSTM(number_of_hidden_units,return_sequences = True,input_shape=(max_length,word_feature_size)))
+    model.add(LSTM(number_of_hidden_units,return_sequences = True))
     return model
-
-
-    
+m = Lstm()
