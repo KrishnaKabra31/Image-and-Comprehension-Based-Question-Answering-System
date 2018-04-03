8c1b226f6b15f77bba570367d12171b3657a365c
Krishna <kanha31@gmail.com>
2018-04-04 00:07:27 +0530

    Minor Bug Fix

--- Lstm.py
+++ Lstm.py
@@ -5,6 +5,6 @@ from keras.layers import LSTM
 def Lstm(word_feature_size = 300,number_of_hidden_units = 1024,max_length = 30):
     model = Sequential()
     model.add(LSTM(number_of_hidden_units,return_sequences = True,input_shape=(max_length,word_feature_size)))
-    model.add(LSTM(number_of_hidden_units,return_sequences = True))
+    model.add(LSTM(number_of_hidden_units,return_sequences = False))
     return model
 m = Lstm()
