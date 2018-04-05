1079c33dd5732a4ddfbcce904f87541ce61da601
Krishna <kanha31@gmail.com>
2018-04-05 13:33:41 +0530

    Modified LSTM to return two layers
    Signed-off-by: Krishna <kanha31@gmail.com>

--- Lstm.py
+++ Lstm.py
@@ -1,10 +1,17 @@
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation,Merge,RepeatVector,concatenate
 from keras.layers import LSTM
-
+from keras import backend as tf
 def Lstm(word_feature_size = 300,number_of_hidden_units = 1024,max_length = 30):
-    model = Sequential()
-    model.add(LSTM(number_of_hidden_units,return_sequences = True,input_shape=(max_length,word_feature_size)))
-    model.add(LSTM(number_of_hidden_units,return_sequences = False))
-    return model
-m = Lstm()
+    left = Sequential()
+    left.add(LSTM(1024, input_shape = (300,30),return_sequences=True))
+    left.add(Dense(1024,activation = "softmax"))
+    right = Sequential()
+    right.add(left)
+    right.add(LSTM(1024,return_sequences=False))
+    print(tf.concatenate([tf.reshape(left.outputs[0][-1][-1],(1024,)),tf.reshape(right.outputs[0],(1024,))],axis=-1))
+    right.add(Dense(1024,activation = "softmax"))
+    return left,right
+m,l = Lstm()
+#print(l)
+#print(m.outputs)
