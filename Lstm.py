26a18b0a6ee59b6bdd0f028ab86356a2f5e8fb62
Krishna <kanha31@gmail.com>
2018-04-09 00:05:02 +0530

    LSTM modified

--- Lstm.py
+++ Lstm.py
@@ -1,17 +1,22 @@
-from keras.models import Sequential
-from keras.layers import Dense, Activation,Merge,RepeatVector,concatenate
+from keras.models import Sequential,Model
+from keras.layers import Dense,Dot,Lambda,Input, Activation,Merge,RepeatVector,Concatenate,Dropout,Reshape
 from keras.layers import LSTM
 from keras import backend as tf
-def Lstm(word_feature_size = 300,number_of_hidden_units = 1024,max_length = 30):
-    left = Sequential()
-    left.add(LSTM(1024, input_shape = (300,30),return_sequences=True))
-    left.add(Dense(1024,activation = "softmax"))
-    right = Sequential()
-    right.add(left)
-    right.add(LSTM(1024,return_sequences=False))
-    print(tf.concatenate([tf.reshape(left.outputs[0][-1][-1],(1024,)),tf.reshape(right.outputs[0],(1024,))],axis=-1))
-    right.add(Dense(1024,activation = "softmax"))
-    return left,right
-m,l = Lstm()
+def Lstm(word_feature_size = 300,number_of_hidden_units = 1024,que_length = 26):#,num_words,embedding_dim):
+  #  left = Sequential()
+    inputs = Input(shape = (26,300))
+    left = LSTM(1024, input_shape = (que_length,word_feature_size),return_sequences=True)(inputs)
+    p = Lambda(lambda x:x[:,-1,:])(left)
+    left = (Dropout(rate=0.4))(left)
+    print(p)
+    left = LSTM(1024,return_sequences=False)(left)
+    left = Lambda(lambda x:x[:,:])(left)
+    left = Dropout(rate = 0.4)(left)
+    print(left)
+    con = Concatenate(axis = 1)([p,left])
+    model = Model(input= inputs, output=con)
+    print(model.output_shape)
+    return con
+m = Lstm()
 #print(l)
 #print(m.outputs)
